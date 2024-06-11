984b2126cd0820eb1c5c47a18eece20e7b6085f5
Armand Faris A Surbakti <actuallyarmand@gmail.com>
2024-06-11 11:55:41 +0700

    Update 2024_06_04_004721_create_answers_table.php  with adding nullable values

--- database/migrations/2024_06_04_004721_create_answers_table.php
+++ database/migrations/2024_06_04_004721_create_answers_table.php
@@ -1,11 +1,6 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Support\Facades\DB;
-
-class CreateAnswersTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -14,11 +9,11 @@ class CreateAnswersTable extends Migration
     {
         Schema::create('answers', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('user_id');
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-            $table->foreignId('question_id')->constrained()->onDelete('cascade');
-            $table->foreignId('option_id')->constrained()->onDelete('cascade');
-            $table->string('answer_value');
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->nullable();
+            $table->foreignId('question_id')->constrained()->onDelete('cascade')->nullable();
+            $table->foreignId('option_id')->constrained()->onDelete('cascade')->nullable();
+            $table->integer('answer_value')->nullable();
             $table->timestamps();
         });
     }
@@ -28,6 +23,6 @@ class CreateAnswersTable extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('answers_');
+        Schema::dropIfExists('answers');
     }
 };
