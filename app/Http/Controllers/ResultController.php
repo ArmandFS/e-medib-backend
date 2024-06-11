6aed974026bc5092f4bc6553554332af2d134634
Armand Faris A Surbakti <actuallyarmand@gmail.com>
2024-06-11 12:08:10 +0700

    Update ResultController.php

--- app/Http/Controllers/ResultController.php
+++ app/Http/Controllers/ResultController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 
 class ResultController extends Controller
 {
+    
+
     public function index()
     {
         $results = Result::get();
@@ -16,9 +18,11 @@ class ResultController extends Controller
     {
         //show results kalo ada user
         $result = Result::with('user')->find($id);
+        
         if (!$result) {
             return response()->json(['message' => 'Result not found'], 404);
         }
         return response()->json($result);
     }
 }
+
